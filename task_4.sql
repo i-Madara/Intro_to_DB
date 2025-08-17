f9dee660da5d3a947a750da5a02b579053664108
i-Madara <dromartaha93@gmail.com>
2025-08-17 23:10:55 +0300

    Add files via upload

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+-- Query to get the full description of the 'books' table
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    CHARACTER_MAXIMUM_LENGTH, 
+    NUMERIC_PRECISION, 
+    NUMERIC_SCALE, 
+    COLUMN_KEY 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = DATABASE() 
+    AND TABLE_NAME = 'Books';
