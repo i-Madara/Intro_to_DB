c20c1ef3024c542998aa30db1c4ab1dbe9825430
i-Madara <dromartaha93@gmail.com>
2025-08-17 23:25:07 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
--- Inserting a single row into the 'Customers' table
+-- Insert a single row into the 'Customers' table in alx_book_store database
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
